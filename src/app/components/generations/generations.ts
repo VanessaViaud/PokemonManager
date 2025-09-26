87d93a18c95de0b5d04591d05c96eee77645bc0d
VanessaViaud <vanessa.viaud@gmail.com>
2025-09-26 09:16:11 +0200

    features/add_components

---
+++ src/app/components/generations/generations.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generations',
+  imports: [],
+  templateUrl: './generations.html',
+  styleUrl: './generations.css'
+})
+export class Generations {
+
+}
